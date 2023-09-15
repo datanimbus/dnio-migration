116225cbb8318137640113e48aa1cbe0af144db0
Jugnu Agrawal <jugnu@appveen.com>
2023-09-15 17:02:18 +0530

    to keep pod up and running

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
-console.log('Container is Read');
+const path = require('path');
+const express = require('express');
+const log4js = require('log4js');
+
+const PORT = process.env.PORT || 3000;
+const LOG_LEVEL = process.env.LOG_LEVEL || 'info';
+
+const logger = log4js.getLogger('Server');
+const app = express();
+
+
+log4js.configure({
+    appenders: { 'out': { type: 'stdout' }, file: { type: 'multiFile', base: 'logs/', property: 'categoryName', extension: '.log', maxLogSize: 10485760, backups: 3, compress: true } },
+    categories: { default: { appenders: ['out', 'file'], level: LOG_LEVEL } }
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+
+app.use((req, res, next) => {
+    logger.info(req.method, req.headers['x-forwarded-for'] || req.connection.remoteAddress, req.path, req.params, req.query, req.body);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Migration pod is up and running' });
+});
+
+app.listen(PORT, (err) => {
+    if (!err) {
+        logger.info('Server is listening on port', PORT);
+    } else {
+        logger.error(err);
+    }
+});
